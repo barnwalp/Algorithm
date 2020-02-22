f68acdd71cffdf1e41ff421c52af391fbd4e4ee7
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-02-22 19:33:50 +0530

    finding maximum digits in given list for radix sort

--- 6. radix_sort.py
+++ 6. radix_sort.py
@@ -1,2 +1,13 @@
-import counting_sort
 
+input_data = [804, 26, 5, 52, 10]
+digit = 0
+print(digit)
+for value in input_data:
+    digit1 = 0
+    while value >= 1:
+        value = value/10
+        digit1 += 1
+    if digit < digit1:
+        digit = digit1
+
+print(digit)
