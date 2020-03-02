82fc5ddc50f76a4b2e395e08fc0d2e283d26dc5c
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-03-02 09:51:08 +0530

    modified radix sort

--- 6. radix_sort.py
+++ 6. radix_sort.py
@@ -1,3 +1,20 @@
+def radix_sort(input_data, digit):
+    while digit > 0:
+        # create an empty list of length 9
+        key = [0] * 10
+        # print(key)
+        for index in (input_data % 10):
+            key[index] = key[index]+1
+        # print(key)
+        output_data = []
+        for index in range(10):
+            while key[index] > 0:
+                output_data.append(index)
+                # print(output_data)
+                key[index] = key[index]-1
+                # print(key[index])
+    return output_data
+
 
 input_data = [804, 26, 5, 52, 10]
 digit = 0
@@ -9,5 +26,5 @@ for value in input_data:
         digit1 += 1
     if digit < digit1:
         digit = digit1
-
-print(digit)
+# print(digit)
+print(radix_sort(input_data, digit))
