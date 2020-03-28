9f1e29bd4800a1fb69dcf65b4ea27749e6cb35a1
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-03-28 20:22:26 +0530

    huffman coding

--- huffman_codes.py
+++ huffman_codes.py
@@ -5,13 +5,17 @@
 #     code = defaultdict(list)
 
 
+
 input_data = "i like huffman codes more than brussels sprouts"
-master_data = ("","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
-"p","q","r","s","t","u","v","w","x","y","z")
-frequency = {}
+master_data = ("", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+               "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+freq = {}
 
 for char in input_data:
-    frequency[char] = input_data.count(char)
+    freq[char] = input_data.count(char)
 
-
+frequency = [(x, y) for (y, x) in freq.items()]
 print(frequency)
+sorted(frequency)
+print(frequency)
+
