a60bb91a3d9da6afe601e0c031045b1797b7b997
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-03-28 13:08:20 +0530

    counting frequency of input string

--- huffman_codes.py
+++ huffman_codes.py
@@ -1,14 +1,19 @@
-import heap_functions
+# def huffman(freqtable):
+#     from collections import defaultdict
+#     from heapq import heappush, heappop, heapify
+#
+#     code = defaultdict(list)
 
 
+input_data = "I like huffman codes more than brussels sprouts"
+master_data = ("","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+"p","q","r","s","t","u","v","w","x","y","z")
+frequency = {}
 
-input_data = [
-    ['a', 45],
-    ['b', 13],
-    ['c', 12],
-    ['d', 16],
-    ['e', 9],
-    ['f', 5]
-]
+for char in master_data:
+    if char in input_data:
+        frequency[char] = input_data.count(char)
+    else:
+        frequency[char] = 0
 
-print(input_data[1])
+print(frequency)
