fc4cb38aeeb245f11bfb4adb4a880337945de0cb
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-06-08 22:34:10 +0530

    style - Added comment

--- prims_algorithm.py
+++ prims_algorithm.py
@@ -15,7 +15,7 @@ def prims(g, edges, vertices):
     print(f'near array is: {near}')
     print(f'edges are: {edges}')
 
-    # finding the edge with lowest weight
+    # finding the first edge with lowest weight
     min_tuple = min(edges, key=itemgetter(2))
     edge = min_tuple[:2]
     # storing the minimum weight edge in minimun spanning tree
@@ -30,16 +30,19 @@ def prims(g, edges, vertices):
     cost = min_tuple[2]
     cost_of_mst += cost
     # print(f'{cost_of_mst} - -> {edges_in_mst}')
-    i = 0
-    while(i < len(vertices)-2):
+
+    # total no of iteration will equal to the minimum no of edges
+    # required to connect all vertices of the graph
+    i = 1
+    while(i < len(vertices)-1):
         min_weight = float("inf")
         for key, value in near.items():
-            # print(f'{key} --> {value}')
-            # if vertex has already been selected in MST, distance
-            # of it in the near dictionary should be zero
+            # if vertex has already been selected for MST, cost of this vertex
+            # should be zero, and should be reflected as such in near dict
             if key in vertices_in_mst:
                 near[key] = 0
             else:
+                # finding the next edge with minimum weight
                 for vertex, weight in g[key].items():
                     if vertex in vertices_in_mst:
                         current_value = near[key]
@@ -54,8 +57,9 @@ def prims(g, edges, vertices):
         mst_edges.append(temp_edge)
         cost_of_mst += temp_weight
         vertices_in_mst.add(temp_vertex)
-        print(f'{mst_edges} --> {cost_of_mst} --> {vertices_in_mst}')
+        # print(f'{mst_edges} --> {cost_of_mst} --> {vertices_in_mst}')
         i += 1
+    # returns both edges and cost as a tuple
     return mst_edges, cost_of_mst
 
 
@@ -69,54 +73,36 @@ if __name__ == "__main__":
         6: {1: 10, 5: 25},
         7: {2: 14, 4: 18, 5: 24}
     }
-    # size = len(g)
 
-    # generating vertices
+# generating vertices and finding cost of vertices
     vertices = g.keys()
-    # print(f'vertices of the graph are: {vertices}')
     no_of_vertices = len(vertices)
 
-    # cost can simply be found using dictionary methods
+# cost can simply be found using dictionary methods
     print(f'cost matrix using dictionary is: {g[1][6]}')
-    # creating an empty cost matrix of size [vertices+1][vertices+1]
-# as list will start with index 0 and vertices start with 1; so to
-# accomodate 7 vertices, 8 indices will be required
-# cost_matrix = [
-#     [float("inf") for _ in range(size+1)]
-#     for _ in range(size+1)
-# ]
-
-# populating the cost matrix with the weight of the edge, cost of
-# the two vertices (say k, l) will be denoted by cost_matrix[k][l]
-# for key, value in g.items():
-#     cost_matrix[key][key] = 0
-#     for adjacent_key, weight in value.items():
-#         # print(f'{adjacent_key} --> {weight}')
-#         cost_matrix[key][adjacent_key] = weight
-# print(cost_matrix)
 
 # generating weighted edges; [(1, 2, 28), (1, 6, 10)....]
-edges = []
-for key, value in g.items():
-    # print(f'{key} --> {value}')
-    for edge, cost in value.items():
-        if {key, edge} not in edges:
-            edges.append({key, edge})
-for index, value in enumerate(edges):
-    edges[index] = list(value)
-    u = edges[index][0]
-    v = edges[index][1]
-    cost = g[u][v]
-    # print(f'{u} --> {v} --> {cost} --> {edges[index]}')
-    edges[index].append(cost)
-    edges[index] = tuple(edges[index])
+    edges = []
+    for key, value in g.items():
+        # print(f'{key} --> {value}')
+        for edge, cost in value.items():
+            if {key, edge} not in edges:
+                edges.append({key, edge})
+    for index, value in enumerate(edges):
+        edges[index] = list(value)
+        u = edges[index][0]
+        v = edges[index][1]
+        cost = g[u][v]
+        # print(f'{u} --> {v} --> {cost} --> {edges[index]}')
+        edges[index].append(cost)
+        edges[index] = tuple(edges[index])
 
 # ceating an empty 2D list of dimension [no_of_verties-1][2]
-edges_in_mst = [
-    [None for _ in range(2)]
-    for _ in range(no_of_vertices-1)
-]
-print(f'Final edges in mst are: {edges_in_mst}')
+    edges_in_mst = [
+        [None for _ in range(2)]
+        for _ in range(no_of_vertices-1)
+    ]
+    print(f'Final edges in mst are: {edges_in_mst}')
 
-final_edges = prims(g, edges, vertices)
-print(f'edges in mst are: {final_edges}')
+    final_edges = prims(g, edges, vertices)
+    print(f'edges in mst are: {final_edges}')
