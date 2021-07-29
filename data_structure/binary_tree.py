105701a987ccdc3162a84fffdb8831ef91259ea2
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-07-29 11:28:43 +0530

    feat/implementing sibling and children method in binary tree

--- data_structure/binary_tree.py
+++ data_structure/binary_tree.py
@@ -15,3 +15,22 @@ class BinaryTree(Tree):
         Return None if p does not have a right child.
         """
         raise NotImplementedError('must be implemented by subclass')
+
+    # ------ concrete methods implemented in this class -----------
+    def sibling(self, p):
+        """Returns a Position representing p's sibling(or None if no sibling)"""
+        parent = self.parent(p)
+        if parent is None:
+            return None
+        else:
+            if p == self.left(parent):
+                return self.right(parent)
+            else:
+                return self.left(parent)
+
+    def children(self, p):
+        """Generate an iteration of Positions representing p's children"""
+        if self.left(p) is not None:
+            return self.left(p)
+        if self.right(p) is not None:
+            return self.right(p)
