648b2096d8ac44885839b4db0498c7b31271e1f9
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-02-02 08:46:17 +0530

    feat/brute force implementation

--- Javascript/water_container.js
+++ Javascript/water_container.js
@@ -16,3 +16,25 @@ const testCase1 = [0,0]                 //0
 const testCase2 = [3,4]                 //3
 const testCase3 = [1,2,3,6,4,7]         //18
 const testCase4 = [1,8,6,2,5,4,8,3,7]   //49
+
+const maxArea = function(height) {
+  area=0;
+  for(p1=0; p1<height.length; p1++) {
+    for(p2=p1+1; p2<height.length; p2++) {
+      temp_area = findArea(height,p1,p2);
+      if (area<temp_area) {
+        area = temp_area;
+      }
+    }
+  }
+  return area;
+}
+
+const findArea = function(height, p1, p2) {
+  base = p2-p1;
+  height = Math.min(height[p1], height[p2]);
+  return base*height;
+}
+
+// console.log(findArea(testCase3, 1, 4));
+console.log(maxArea(testCase4));
