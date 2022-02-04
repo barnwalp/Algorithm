da16f3bf3bbf288ab7e87d1c20a285bfd795ef3f
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-02-04 21:19:03 +0530

    feat/trapping rain water function with error

--- Javascript/trapping_rain.js
+++ Javascript/trapping_rain.js
@@ -12,3 +12,45 @@ let testCase1 = [1];                          //0
 let testCase2 = [2,3,2,1];                    //0
 let testCase3 = [1,2];                        //0
 let testCase4 = [0,1,0,2,1,0,1,3,2,1,2,1];    //6
+let testCase5 = [3,2,1,2,4];                  //6
+let testCase6 = [2,1,2];                      //1
+
+const trap_rain = function(height) {
+  // lets modify the array to remove descending values at the end
+  let p = height.length-1;
+  while(p>0) {
+    if(height[p]<=height[p-1]) {
+      height.pop();
+    } else { 
+      break;
+    }
+    p--;
+  }
+
+  p=1;
+  let pointer=0; tempArea=0; area=0;
+  while(p<height.length-1) {
+    if(height[p]>height[pointer]) {
+      pointer=p;
+      p++;
+      area += tempArea;
+      tempArea=0;
+    } else {
+      tempArea += height[pointer]-height[p];
+      p++;
+      if(p==height.length-1) {
+        let offset = height[pointer]-height[p];
+        p=pointer+1;
+        while(p<height.length-1) {
+          if((height[pointer]-height[p]-offset)>0) {
+            area += height[pointer]-height[p]-offset;
+            p++;
+          }
+        }
+      }
+    }
+  }
+  return area;
+}
+
+console.log(trap_rain(testCase6));
