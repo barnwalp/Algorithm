59dc3042e26a8c82dcb928e96e7d0ee259a138ca
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-02-04 07:38:10 +0530

    feat/problem constraint and testcases for trapping rain water

---
+++ Javascript/trapping_rain.js
@@ -0,0 +1,14 @@
+// Problem:
+// Given n non-negative integers representing an elevation map where the width of
+// each bar is 1, compute how much water it can trap after raining.
+
+// Constraint:
+// 1. y-axis not to be considered for trapping water.
+// 2. Array contains only not-negative numbers.
+// 3. Minimum 1 number is present in the array
+
+// Testcases:
+let testCase1 = [1];                          //0
+let testCase2 = [2,3,2,1];                    //0
+let testCase3 = [1,2];                        //0
+let testCase4 = [0,1,0,2,1,0,1,3,2,1,2,1];    //6
