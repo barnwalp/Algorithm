ad8e944f4805331a703246992896e7f7b0a9ae24
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-02-20 11:38:36 +0530

    feat/added problem constraint and test case for longest substring problem

---
+++ Javascript/longest_substring.js
@@ -0,0 +1,16 @@
+// Problem:
+// Given a string s, find the length of the longest substring without repeating characters.
+
+// Constraints:
+// 1. 0 <= s.length <= 5 * 104
+// 2. s consists of English letters, digits, symbols and spaces.
+// 3. lower and upper case letters will be considered as different characters.
+
+// TestCases:
+// let s='';                 //0
+// let s='a';                //1
+// let s='aaa';              //1
+// let s='pqrsptuvqsnoppq';  //8
+// let s='pwwkew';           //3
+// let s='abcAd';            //5
+
