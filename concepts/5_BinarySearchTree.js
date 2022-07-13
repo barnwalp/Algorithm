1daf2c2250e00a9133955c1363d9f937bcda89f0
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-07-13 22:00:16 +0530

    docs/deleted content of remove method

--- concepts/5_BinarySearchTree.js
+++ concepts/5_BinarySearchTree.js
@@ -57,81 +57,10 @@ class BinarySearchTree {
   }
 
 	remove(value) {
-		if(!this.root) {
-			return false;
-		}
-		let leader = this.root;
-		let parent = null;
-		// console.log(leader);
-		// find the lookup value in the bst
-		while (true) {
-			if (value > leader.value) {
-				parent = leader;
-				leader = leader.right;
-			} else if (value < leader.value) {
-				parent = leader;
-				leader = leader.left;
-				// when lookup value is found
-			} else if (value === leader.value){
-				// if the lookupnode is a leaf node
-				if (!leader.left && !leader.right) {
-					// check if leaf is left or right child of parent
-					if (leader.value > parent.value) {
-						parent.right = null;
-					} else {
-						parent.left = null;
-					}
-					return true;
-					// if the lookupnode has only left child
-				} else if (leader.left && !leader.right) {
-					parent.left = leader.left;
-					leader.left = null;
-					return true;
-					// if the lookupnode has only right child
-				} else if (leader.right && !leader.left) {
-					parent.right = leader.right;
-					leader.right = null;
-					return true;
-					// if the lookupnode has both child
-				} else if (leader.left && leader.right) {
-					// step 1: find the lowest value node in right sub-tree
-					// let smallestnode = leader;
-					// let curnode = leader;
-					// let curparent = parent;
-					// let parentofsmallestnode = curparent;
-					// while(curnode) {
-					// 	if (curnode.value < smallestnode.value) {
-					// 		smallestnode = curnode;
-					// 		parentofsmallestnode = curparent;
-					// 	};
-					// 	curparent = curnode;
-					// 	curnode = curnode.right;
-					// }
-					// // step 2: replace the node value with lowest node 
-					// // of right sub-tree, say n
-					// leader.value = smallestnode.value;
-
-					// // Step 3: remove the lowest node of right sub-tree, N
-					// // Since it will have only one right child, same algo can be used
-					// parentOfSmallestNode.left = smallestNode.right;					
-					// smallestNode.right = null;
-					let curParent = leader;
-					let curNode = leader.right;
-					while (curNode.left) {
-						curParent = curNode;
-						curNode = curNode.left;
-					}
-					leader.value = curNode.value;
-					if (curNode.right) {
-						curParent.left = curNode.right;
-						curNode.right = null;
-					} else {
-						curParent.left = null;
-
-					}
-				}
-			}
-		}
+		// step 1: traverse to the node which is to be removed
+		// step 2: find the lowest value node in right sub-tree
+		// step 3: Replace the node value with lowest node of right sub-tree
+		// step 4: remove the lowest node of right sub-tree which have only one right child
 	}
 
 	breadthFirstSearch() {
