fe174f597aed1cb29a2e928dc0343e213f171098
Pankaj Barnwal <suno.pankaj@gmail.com>
2023-04-21 20:21:13 +0530

    feat/rotate matrix with error

--- Javascript/Array/ctci/rotateMatrix.js
+++ Javascript/Array/ctci/rotateMatrix.js
@@ -16,24 +16,58 @@ const testCase1 = [
 	[7,8,9],
 ]
 const testCase2 = [
-	[1,2,3,4],
-	[5,6,7,8],
+	[1, 2, 3, 4],
+	[5, 6, 7, 8],
 	[9,10,11,12],
 	[13,14,15,16],
 ]
 
-function rotateMatrix(matrix) {
-	let newMatrix = matrix;
+const testCase3 = [
+	[1, 2, 3, 4, 5],
+	[6, 7, 8, 9, 10],
+	[11,12,13,14,15],
+	[16,17,18,19,20],
+	[21,22,23,24,25]
+]
+console.log(testCase2);
+console.log(testCase3);
+
+function rotateMatrix(ipMatrix) {
+	let matrix = ipMatrix.slice();
+	let layers = Math.ceil(matrix.length/2)	
+	for (let layer = 0; layer <= layers; layer++) {
+		let first = layer;
+		let last = matrix.length-layer-1;
+		for (let i=first; i<=last; i++) {
+			let offset = i - first;
+			// save top
+			let temp = matrix[first][i];
+			// left -> top
+			matrix[first][i] = matrix[last-offset][first]
+			// bottom -> left
+			matrix[last-offset][first] = matrix[last][last-offset]
+			// right -> bottom
+			matrix[last][last-offset] = matrix[i][last]
+			// top -> right
+			matrix[i][last] = temp;
+		}
+
+	}
+	return matrix;
+}
+
+function transposeMatrix(matrix) {
+	let newMatrix = matrix.slice();
 	for (i = 0; i < matrix.length; i++) {
 		for (j = 0; j < matrix[0].length; j++) {
 			newMatrix[i][j] = matrix[j][i];
-			// console.log(i, j, newMatrix[j][i], matrix[i][j]);
 		}
 	}
 	return newMatrix;
 }
 
-console.log(rotateMatrix(testCase1), testCase1);
-console.log(rotateMatrix(testCase2), testCase2);
-// rotateMatrix(testCase1);
-// rotateMatrix(testCase2);
+// console.log(transposeMatrix(testCase1), testCase1);
+// console.log(transposeMatrix(testCase2), testCase2);
+// console.log(rotateMatrix(testCase1), testCase1);
+console.log(rotateMatrix(testCase2));
+console.log(rotateMatrix(testCase3));
